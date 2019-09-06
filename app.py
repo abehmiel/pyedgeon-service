ffab2ef5f47dce2914347de61b68eca45176a595
abehmiel <abehmiel@gmail.com>
2019-09-05 23:57:04 -0400

    init imgur_lib, app, validate_sms

---
+++ app.py
@@ -0,0 +1,42 @@
+#Service that receives SMS from twilio and turns the body text into an optical
+# illusion button with pyedgeon, then uploading that image to imgur, using the
+# public imgur link as an image source, and then sending that image via MMS to
+# the client
+
+import requests
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
+from pyedgeon import Pyedgeon
+from validate_sms import validate_sms
+from pathlib import Path
+from imgur_lib import *
+
+app = Flask(__name__)
+
+# twilio credential go here
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+    """Respond to incoming with a simple text message."""
+
+    resp = MessagingResponse()
+    # validate input and prepare txt, msg_id
+    clean_request = validate_sms(request)
+    text = clean_request.values['body']
+    msg_id = clean_request.values['sid']
+    # create illusion object & save to file
+    ill = Pyedgeon(illusion_text=text,
+                   file_path='static/',
+                   filename=msg_id,
+                   file_ext='.png')
+    ill.create()
+    # take created file and push to imgur anonymously
+    res = img_upload(ill.get_file_path(), ill.illusion_text, msg_id)
+    img_link = res["link"]
+    resp.messages(body="Thanks for using pyedgeon!",
+                  media_url=img_link)
+
+    return resp
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ imgur_lib.py
@@ -0,0 +1,20 @@
+from imgurpython import ImgurClient
+import urllib2
+import os
+import json
+
+client_id = '72437224179f262'
+client_secret = 'e57c65c24a3c118921aec91b4cf65222a43184a3'
+imgur_endpoint = 'https://api.imgur.com/3/'
+
+def img_upload(file_path, text, msg_id):
+
+	client = ImgurClient(client_id, client_secret)
+
+	conf = {"album" : "pyedgeon-service-anon",
+		 "description": text,
+		 "title" : msg_id}
+	res = client.upload_from_path(file_path, config = conf, anon = True)
+	#print(res)
+	#print(res["link"])
+    return res
