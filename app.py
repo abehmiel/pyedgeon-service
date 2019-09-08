f09150a4c15a355f628de2cbf1295d6bb38bb4d4
abehmiel <abehmiel@gmail.com>
2019-09-08 13:08:53 -0400

    improve error handling

--- app.py
+++ app.py
@@ -28,19 +28,22 @@ app.debug = True
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
     """Respond to incoming with a simple text message."""
-
+    # create MessagingResponse object and cast request to dict
     resp = MessagingResponse()
-    # validate input and prepare txt, msg_id
     rq = request.form.to_dict()
     logging.info(str(rq))
+    # validate input and prepare txt, msg_id
+    val = validate_sms(rq)
     try:
-        clean_request = validate_sms(rq)
-    except:
-        # TODO: handle errors
-        pass
+        assert val['success'] == True
+    except AssertionError as e:
+        # handle case where there is an invalid input
+        msg = resp.message(val['error'])
+        return str(resp)
 
-    text = clean_request['Body']
-    msg_id = clean_request['MessageSid']
+    # input is validated, proceed with Pyedgeon
+    text = rq['Body']
+    msg_id = rq['MessageSid']
     # create illusion object & save to file
     ill = Pyedgeon(illusion_text=text,
                    file_path='static/',
@@ -50,14 +53,14 @@ def sms_reply():
     # take created file and push to imgur anonymously
     res = img_upload(ill.get_file_path(), ill.illusion_text, msg_id)
     img_link = str(res["link"])
-    msg = resp.message("Thanks for using pyedgeon!")
+    msg = resp.message("Thanks for using pyedgeon-service!")
     msg.media(img_link)
     logging.info('link:{link} text:{text} file:{fi} from:{fr}, to:{to} sid:{s}'
                  .format(link=img_link,
                          text=text,
                          fi=ill.get_file_path(),
-                         fr=clean_request['From'],
-                         to=clean_request['To'],
+                         fr=rq['From'],
+                         to=rq['To'],
                          s=msg_id,
                          )
                  )
--- src/validate_sms.py
+++ src/validate_sms.py
@@ -2,25 +2,43 @@ from flask import request
 import logging
 
 def validate_sms(request):
-    """validate and clean data from SMS message"""
+    """validate and clean data from SMS message.
+    Input -> Dict of str:str
+    Output -> Dict of {str: Bool, str:str}
+    If validated, output . Otherwise, contains 'error' field and msg
+    """
+
     try:
         text = request['Body']
-    except:
-        logging.warning('no body in request')
+    except IndexError as e:
+        err = 'No "Body" field in request'
+        errdict = {'validated': False, 'error': err}
+        logging.warning(err)
+        return errdict
+
     try:
         assert isinstance(text, str)
-    except:
-        logging.warning('input is not a string')
+    except AssertionError as e:
+        err = 'Input is not a string'
+        errdict = {'validated': False, 'error': err}
+        logging.warning(err)
+        return errdict
 
     try:
         assert len(text) > 20
-    except:
-        logging.warning('text is longer than 20 characters')
+    except AssertionError as e:
+        err = 'Text is longer than 20 characters. Try a shorter phrase.'
+        errdict = {'validated': False, 'error': err}
+        logging.warning(err)
+        return errdict
 
     try:
         assert len(text) < 6
-    except:
-        logging.warning('text is shorter than 6 characters')
+    except AssertionError as e:
+        err = 'Text is shorter than 6 characters. Try a longer phrase.'
+        errdict = {'validated': False, 'error': err}
+        logging.warning(err)
+        return errdict
 
-    return request
+    return {'validated': True, 'error': 'OK'}
 
